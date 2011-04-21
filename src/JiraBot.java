62c9121086734167a0e55637f57420429d421831
Piotr Wach <wach.piotrek@gmail.com>
2011-04-21 10:16:49 +0200

    Small change

--- src/JiraBot.java
+++ src/JiraBot.java
@@ -29,59 +29,62 @@ public class JiraBot extends PircBot {
 		if(tokens.length > 0) {
 			if(tokens.length > 1) {
 				try {
-				Issue issue = jiraClient.getIssueClient().getIssue(tokens[1], pm);
-					if(issue != null) {
-						if(tokens[0].equals("!summary")) {
-							sendMessage(channel, issue.getSummary()); 
-						} else if(tokens[0].equals("!status")) {
-							sendMessage(channel, issue.getStatus().getName());
-						} else if(tokens[0].equals("!url")) {
-							sendMessage(channel, issue.getSelf().toString());
-						} else if(tokens[0].equals("!desc")) {
-							String desc = issue.getField("description").getValue().toString();
-							sendLongMessage(channel, desc);
-						} else if(tokens[0].equals("!field") && tokens.length > 2) {
-							Field field = issue.getField(tokens[2]);
-							if(field != null && field.getValue() != null) {
-								sendLongMessage(channel, field.getValue().toString());
-							} else {
-								sendMessage(channel, format(Colors.RED, "Error: ") + tokens[2] + " field not found");
-							}
-						} else if(tokens[0].equals("!assignee")) {
-							sendMessage(channel, issue.getAssignee().getDisplayName());
-						} else if(tokens[0].equals("!reporter")) {
-							sendMessage(channel, issue.getReporter().getDisplayName());
-						} else if(tokens[0].equals("!issue")) {
-							String reply = MESSAGE_SEPARATOR + "\n";
-							reply += format(Colors.BOLD, "Summary: ") + issue.getSummary() + "\n";
-							reply += format(Colors.BOLD, "Status: ") + issue.getStatus().getName() + "\n";
-							reply += format(Colors.BOLD, "Reporter: ") + issue.getReporter().getDisplayName() 
-								+ format(Colors.BOLD, " Assignee: ") + issue.getAssignee().getDisplayName() + "\n";
-							reply += format(Colors.BOLD, "Description: ") + issue.getField("description").getValue().toString() + "\n";
-							
-							Iterator<Comment> iterator = issue.getComments().iterator();
-							Comment lastComment = null;
-							while(iterator.hasNext()) {
-								Comment comment = iterator.next();
-								if(lastComment != null && 
-										comment.getCreationDate().compareTo(
-												lastComment.getCreationDate()) > 0) {
-									lastComment = comment;
+					if(tokens[0].startsWith("!")) {
+						Issue issue = jiraClient.getIssueClient().getIssue(tokens[1], pm);
+						if(issue != null) {
+							if(tokens[0].equals("!summary")) {
+								sendMessage(channel, issue.getSummary()); 
+							} else if(tokens[0].equals("!status")) {
+								sendMessage(channel, issue.getStatus().getName());
+							} else if(tokens[0].equals("!url")) {
+								sendMessage(channel, issue.getSelf().toString());
+							} else if(tokens[0].equals("!desc")) {
+								String desc = issue.getField("description").getValue().toString();
+								sendLongMessage(channel, desc);
+							} else if(tokens[0].equals("!field") && tokens.length > 2) {
+								Field field = issue.getField(tokens[2]);
+								if(field != null && field.getValue() != null) {
+									sendLongMessage(channel, field.getValue().toString());
+								} else {
+									sendMessage(channel, format(Colors.RED, "Error: ") + tokens[2] + " field not found");
 								}
+							} else if(tokens[0].equals("!assignee")) {
+								sendMessage(channel, issue.getAssignee().getDisplayName());
+							} else if(tokens[0].equals("!reporter")) {
+								sendMessage(channel, issue.getReporter().getDisplayName());
+							} else if(tokens[0].equals("!issue")) {
+								String reply = MESSAGE_SEPARATOR + "\n";
+								reply += format(Colors.BOLD, "Summary: ") + issue.getSummary() + "\n";
+								reply += format(Colors.BOLD, "Status: ") + issue.getStatus().getName() + "\n";
+								reply += format(Colors.BOLD, "Reporter: ") + issue.getReporter().getDisplayName() 
+									+ format(Colors.BOLD, " Assignee: ") + issue.getAssignee().getDisplayName() + "\n";
+								if(issue.getField("description") != null && issue.getField("description").getValue() != null) {
+									reply += format(Colors.BOLD, "Description: ") + issue.getField("description").getValue().toString() + "\n";
+								}
+								Iterator<Comment> iterator = issue.getComments().iterator();
+								Comment lastComment = null;
+								while(iterator.hasNext()) {
+									Comment comment = iterator.next();
+									if(lastComment != null && 
+											comment.getCreationDate().compareTo(
+													lastComment.getCreationDate()) > 0) {
+										lastComment = comment;
+									}
+								}
+								if(lastComment != null) {
+									reply += format(Colors.BOLD, "Last comment: ") + "(" + lastComment.getAuthor().getDisplayName() + ")" 
+										+ lastComment.getBody() + "\n";
+								} else {
+									reply += format(Colors.BOLD, "Last comment: ") + "no comments yet\n";
+								}
+								
+								reply += MESSAGE_SEPARATOR;
+								
+								sendLongMessage(channel, reply);
 							}
-							if(lastComment != null) {
-								reply += format(Colors.BOLD, "Last comment: ") + "(" + lastComment.getAuthor().getDisplayName() + ")" 
-									+ lastComment.getBody() + "\n";
-							} else {
-								reply += format(Colors.BOLD, "Last comment: ") + "no comments yet\n";
-							}
-							
-							reply += MESSAGE_SEPARATOR;
-							
-							sendLongMessage(channel, reply);
+						} else {
+							sendMessage(channel, "Error: Issue " + tokens[1] + " not found");
 						}
-					} else {
-						sendMessage(channel, "Error: Issue " + tokens[1] + " not found");
 					}
 				} catch(RestClientException e) {
 					sendMessage(channel, format(Colors.RED, "Error: ") + e.getMessage());
